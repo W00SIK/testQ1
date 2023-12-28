6f4342b44129193219513a570a12c08630bcd6cf
unknown <rhrod123456@gmail.com>
2023-12-28 10:54:38 +0900

    finish

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
+
 function App() {
+
+  const [todo, setTodo] = useState([
+    { todoId: 1, todo: "react를 배워봅시다." }
+  ])
+
+  const [goal, setGoal] = useState()
+
+  const addBtnHendler = () => {
+    const newTodo = {
+      id: todo.length + 1,
+      todo: goal,
+    }
+    setTodo([...todo, newTodo])
+    setGoal("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="inputBox">
+        <input
+          value={goal}
+          onChange={(event) => {
+            setGoal(event.target.value)
+          }}
+        />
+        <button onClick={addBtnHendler}>
+          추가하기
+        </button>
+      </div>
+      <div className="mainHeader">
+        <div className="headFont">Todo List</div>
+      </div>
+      <div className="todoBoxForm">
+        {
+          todo.map((item) => {
+            return (
+              <div key={item.todoId} className="todoBox">{item.todo}</div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
